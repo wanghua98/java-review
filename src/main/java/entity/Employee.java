dcf87dd92741f3fdb876ebba5c073b7c3cf1e8a4
Yangfeng <xf935@qq.com>
2026-07-02 14:23:13 +0800

    IO流的学习和使用

---
+++ src/main/java/entity/Employee.java
@@ -0,0 +1,22 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class Employee {
+    private String name;
+    private int salary;
+    private int year;
+    private int month;
+    private int day;
+
+
+    public LocalDate getHireDate() {
+        return LocalDate.of(this.year, this.month, this.day);
+    }
+}
+
